9a6c19244d695d25e466ce0cdff335c1cbb3038d
andriy <evicandrij@gmail.com>
2023-08-27 18:57:29 +0300

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import { Component } from 'react';
+
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
